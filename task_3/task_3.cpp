c2e35f6b45c644ed30f5ee6422d4a9b3c13a6aee
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-18 17:35:34 +0200

    Add project files.

---
+++ task_3/task_3.cpp
@@ -0,0 +1,18 @@
+﻿/*
+3. Написать программу, которая выводят на экран ромб. При этом
+пользователь вводит диагональ ромба.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, ""); // работает только с потоком вывода
+
+
+
+	return 0;
+}
---
+++ task_5/task_5.cpp
@@ -0,0 +1,18 @@
+﻿/*
+5. Написать программу, которая выводит на экран следующее
+изображение.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, ""); // работает только с потоком вывода
+
+
+
+	return 0;
+}
